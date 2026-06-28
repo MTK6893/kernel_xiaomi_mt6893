66a58a498e9b04bc6cd695263079d1303387d277
tiffanyy <yangfanyan7@gmail.com>
2026-06-28 22:57:57 +0800

    Revert "drivers: misc/mtk: base: power: Silence debug and verbose logging"
    This reverts commit 9f13dd6a6af02f62360583ae323bd0d64f79ff9e.

--- drivers/misc/mediatek/base/power/spm_v4/mtk_spm_internal.c
+++ drivers/misc/mediatek/base/power/spm_v4/mtk_spm_internal.c
@@ -218,7 +218,13 @@ void __spm_get_wakeup_status(struct wake_status *wakesta)
 	wakesta->isr = spm_read(SPM_IRQ_STA);
 }
 
-#define spm_print(suspend, fmt, args...)
+#define spm_print(suspend, fmt, args...)	\
+do {						\
+	if (!suspend)				\
+		spm_debug(fmt, ##args);		\
+	else					\
+		spm_crit2(fmt, ##args);		\
+} while (0)
 
 void rekick_vcorefs_scenario(void)
 {
