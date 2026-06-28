437679239420018874928d97958608aa642f2cf5
tiffanyy <yangfanyan7@gmail.com>
2026-06-28 22:55:45 +0800

    Revert "drivers: connectivity: gen4m: Silence more debug logging"
    This reverts commit dfd8417a930b288bdbbdcf892962de1c17b71e4c.

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
@@ -9887,7 +9887,6 @@ void kalUpdateCompHdlrRec(IN struct ADAPTER *prAdapter,
 					% OID_HDLR_REC_NUM;
 }
 
-#if (BUILD_QA_DBG == 1)
 void kalPrintUTC(char *msg_buf, int msg_buf_size)
 {
 	int ret = 0;
@@ -9970,7 +9969,7 @@ void kalPrintLog(const char *fmt, ...)
 
 	va_end(args);
 }
-#endif
+
 
 #if (CFG_SUPPORT_POWER_THROTTLING == 1)
 void kalPwrLevelHdlrRegister(IN struct ADAPTER *prAdapter,
