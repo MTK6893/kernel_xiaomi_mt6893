59d4a3adfe919a9075c1557a7db7eeb28e75eed7
tiffanyy <yangfanyan7@gmail.com>
2026-06-28 23:00:14 +0800

    Revert "[ALPS07919940] GPU: GPUSWERRATA-1566 for CVE-2022-46781"
    This reverts commit f9fc30b6e032a176022ce8caedbce6679e6fe283.

--- drivers/misc/mediatek/gpu/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_kcpu.c
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_kcpu.c
@@ -755,14 +755,13 @@ static int kbase_kcpu_cqs_wait_process(struct kbase_device *kbdev,
 				return -EINVAL;
 			}
 
-			sig_set =
-				evt[BASEP_EVENT32_VAL_OFFSET / sizeof(u32)] > cqs_wait->objs[i].val;
+			sig_set = evt[BASEP_EVENT_VAL_INDEX] > cqs_wait->objs[i].val;
 			if (sig_set) {
 				bool error = false;
 
 				bitmap_set(cqs_wait->signaled, i, 1);
 				if ((cqs_wait->inherit_err_flags & (1U << i)) &&
-				    evt[BASEP_EVENT32_ERR_OFFSET / sizeof(u32)] > 0) {
+				    evt[BASEP_EVENT_ERR_INDEX] > 0) {
 					queue->has_error = true;
 					error = true;
 				}
@@ -772,7 +771,8 @@ static int kbase_kcpu_cqs_wait_process(struct kbase_device *kbdev,
 						error);
 
 				KBASE_TLSTREAM_TL_KBASE_KCPUQUEUE_EXECUTE_CQS_WAIT_END(
-					kbdev, queue, evt[BASEP_EVENT32_ERR_OFFSET / sizeof(u32)]);
+					kbdev, queue,
+					evt[BASEP_EVENT_ERR_INDEX]);
 				queue->command_started = false;
 			}
 
@@ -789,34 +789,12 @@ static int kbase_kcpu_cqs_wait_process(struct kbase_device *kbdev,
 	return bitmap_full(cqs_wait->signaled, cqs_wait->nr_objs);
 }
 
-static inline bool kbase_kcpu_cqs_is_data_type_valid(u8 data_type)
-{
-	return data_type == BASEP_CQS_DATA_TYPE_U32 || data_type == BASEP_CQS_DATA_TYPE_U64;
-}
-
-static inline bool kbase_kcpu_cqs_is_aligned(u64 addr, u8 data_type)
-{
-	BUILD_BUG_ON(BASEP_EVENT32_ALIGN_BYTES != BASEP_EVENT32_SIZE_BYTES);
-	BUILD_BUG_ON(BASEP_EVENT64_ALIGN_BYTES != BASEP_EVENT64_SIZE_BYTES);
-	WARN_ON(!kbase_kcpu_cqs_is_data_type_valid(data_type));
-
-	switch (data_type) {
-	default:
-		return false;
-	case BASEP_CQS_DATA_TYPE_U32:
-		return (addr & (BASEP_EVENT32_ALIGN_BYTES - 1)) == 0;
-	case BASEP_CQS_DATA_TYPE_U64:
-		return (addr & (BASEP_EVENT64_ALIGN_BYTES - 1)) == 0;
-	}
-}
-
 static int kbase_kcpu_cqs_wait_prepare(struct kbase_kcpu_command_queue *queue,
 		struct base_kcpu_command_cqs_wait_info *cqs_wait_info,
 		struct kbase_kcpu_command *current_command)
 {
 	struct base_cqs_wait_info *objs;
 	unsigned int nr_objs = cqs_wait_info->nr_objs;
-	unsigned int i;
 
 	lockdep_assert_held(&queue->kctx->csf.kcpu_queues.lock);
 
@@ -834,17 +812,6 @@ static int kbase_kcpu_cqs_wait_prepare(struct kbase_kcpu_command_queue *queue,
 			nr_objs * sizeof(*objs))) {
 		kfree(objs);
 		return -ENOMEM;
-	}
-
-	/* Check the CQS objects as early as possible. By checking their alignment
-	 * (required alignment equals to size for Sync32 and Sync64 objects), we can
-	 * prevent overrunning the supplied event page.
-	 */
-	for (i = 0; i < nr_objs; i++) {
-		if (!kbase_kcpu_cqs_is_aligned(objs[i].addr, BASEP_CQS_DATA_TYPE_U32)) {
-			kfree(objs);
-			return -EINVAL;
-		}
 	}
 
 	if (++queue->cqs_wait_count == 1) {
@@ -903,13 +870,14 @@ static void kbase_kcpu_cqs_set_process(struct kbase_device *kbdev,
 				"Sync memory %llx already freed", cqs_set->objs[i].addr);
 			queue->has_error = true;
 		} else {
-			evt[BASEP_EVENT32_ERR_OFFSET / sizeof(u32)] = queue->has_error;
+			evt[BASEP_EVENT_ERR_INDEX] = queue->has_error;
 			/* Set to signaled */
-			evt[BASEP_EVENT32_VAL_OFFSET / sizeof(u32)]++;
+			evt[BASEP_EVENT_VAL_INDEX]++;
 			kbase_phy_alloc_mapping_put(queue->kctx, mapping);
 
-			KBASE_KTRACE_ADD_CSF_KCPU(kbdev, CQS_SET, queue, cqs_set->objs[i].addr,
-						  evt[BASEP_EVENT32_ERR_OFFSET / sizeof(u32)]);
+			KBASE_KTRACE_ADD_CSF_KCPU(kbdev, CQS_SET,
+					queue, cqs_set->objs[i].addr,
+					evt[BASEP_EVENT_ERR_INDEX]);
 		}
 	}
 
@@ -927,7 +895,6 @@ static int kbase_kcpu_cqs_set_prepare(
 	struct kbase_context *const kctx = kcpu_queue->kctx;
 	struct base_cqs_set *objs;
 	unsigned int nr_objs = cqs_set_info->nr_objs;
-	unsigned int i;
 
 	lockdep_assert_held(&kctx->csf.kcpu_queues.lock);
 
@@ -945,17 +912,6 @@ static int kbase_kcpu_cqs_set_prepare(
 			nr_objs * sizeof(*objs))) {
 		kfree(objs);
 		return -ENOMEM;
-	}
-
-	/* Check the CQS objects as early as possible. By checking their alignment
-	 * (required alignment equals to size for Sync32 and Sync64 objects), we can
-	 * prevent overrunning the supplied event page.
-	 */
-	for (i = 0; i < nr_objs; i++) {
-		if (!kbase_kcpu_cqs_is_aligned(objs[i].addr, BASEP_CQS_DATA_TYPE_U32)) {
-			kfree(objs);
-			return -EINVAL;
-		}
 	}
 
 	current_command->type = BASE_KCPU_COMMAND_TYPE_CQS_SET;
@@ -1000,9 +956,8 @@ static int kbase_kcpu_cqs_wait_operation_process(struct kbase_device *kbdev,
 		if (!test_bit(i, cqs_wait_operation->signaled)) {
 			struct kbase_vmap_struct *mapping;
 			bool sig_set;
-			uintptr_t evt = (uintptr_t)kbase_phy_alloc_mapping_get(
-				queue->kctx, cqs_wait_operation->objs[i].addr, &mapping);
-			u64 val = 0;
+			u64 *evt = (u64 *)kbase_phy_alloc_mapping_get(queue->kctx,
+						cqs_wait_operation->objs[i].addr, &mapping);
 
 			/* GPUCORE-28172 RDT to review */
 			if (!queue->command_started)
@@ -1015,29 +970,12 @@ static int kbase_kcpu_cqs_wait_operation_process(struct kbase_device *kbdev,
 				return -EINVAL;
 			}
 
-			switch (cqs_wait_operation->objs[i].data_type) {
-			default:
-				WARN_ON(!kbase_kcpu_cqs_is_data_type_valid(
-					cqs_wait_operation->objs[i].data_type));
-				kbase_phy_alloc_mapping_put(queue->kctx, mapping);
-				queue->has_error = true;
-				return -EINVAL;
-			case BASEP_CQS_DATA_TYPE_U32:
-				val = *(u32 *)evt;
-				evt += BASEP_EVENT32_ERR_OFFSET - BASEP_EVENT32_VAL_OFFSET;
-				break;
-			case BASEP_CQS_DATA_TYPE_U64:
-				val = *(u64 *)evt;
-				evt += BASEP_EVENT64_ERR_OFFSET - BASEP_EVENT64_VAL_OFFSET;
-				break;
-			}
-
 			switch (cqs_wait_operation->objs[i].operation) {
 			case BASEP_CQS_WAIT_OPERATION_LE:
-				sig_set = val <= cqs_wait_operation->objs[i].val;
+				sig_set = *evt <= cqs_wait_operation->objs[i].val;
 				break;
 			case BASEP_CQS_WAIT_OPERATION_GT:
-				sig_set = val > cqs_wait_operation->objs[i].val;
+				sig_set = *evt > cqs_wait_operation->objs[i].val;
 				break;
 			default:
 				dev_warn(kbdev->dev,
@@ -1049,10 +987,23 @@ static int kbase_kcpu_cqs_wait_operation_process(struct kbase_device *kbdev,
 				return -EINVAL;
 			}
 
+			/* Increment evt up to the error_state value depending on the CQS data type */
+			switch (cqs_wait_operation->objs[i].data_type) {
+			default:
+				dev_warn(kbdev->dev, "Unreachable data_type=%d", cqs_wait_operation->objs[i].data_type);
+			/* Fallthrough - hint to compiler that there's really only 2 options at present */
+			case BASEP_CQS_DATA_TYPE_U32:
+				evt = (u64 *)((u8 *)evt + sizeof(u32));
+				break;
+			case BASEP_CQS_DATA_TYPE_U64:
+				evt = (u64 *)((u8 *)evt + sizeof(u64));
+				break;
+			}
+
 			if (sig_set) {
 				bitmap_set(cqs_wait_operation->signaled, i, 1);
 				if ((cqs_wait_operation->inherit_err_flags & (1U << i)) &&
-				    *(u32 *)evt > 0) {
+				    *evt > 0) {
 					queue->has_error = true;
 				}
 
@@ -1080,7 +1031,6 @@ static int kbase_kcpu_cqs_wait_operation_prepare(struct kbase_kcpu_command_queue
 {
 	struct base_cqs_wait_operation_info *objs;
 	unsigned int nr_objs = cqs_wait_operation_info->nr_objs;
-	unsigned int i;
 
 	lockdep_assert_held(&queue->kctx->csf.kcpu_queues.lock);
 
@@ -1098,18 +1048,6 @@ static int kbase_kcpu_cqs_wait_operation_prepare(struct kbase_kcpu_command_queue
 			nr_objs * sizeof(*objs))) {
 		kfree(objs);
 		return -ENOMEM;
-	}
-
-	/* Check the CQS objects as early as possible. By checking their alignment
-	 * (required alignment equals to size for Sync32 and Sync64 objects), we can
-	 * prevent overrunning the supplied event page.
-	 */
-	for (i = 0; i < nr_objs; i++) {
-		if (!kbase_kcpu_cqs_is_data_type_valid(objs[i].data_type) ||
-		    !kbase_kcpu_cqs_is_aligned(objs[i].addr, objs[i].data_type)) {
-			kfree(objs);
-			return -EINVAL;
-		}
 	}
 
 	if (++queue->cqs_wait_count == 1) {
@@ -1142,44 +1080,6 @@ static int kbase_kcpu_cqs_wait_operation_prepare(struct kbase_kcpu_command_queue
 	return 0;
 }
 
-static void kbasep_kcpu_cqs_do_set_operation_32(struct kbase_kcpu_command_queue *queue,
-						uintptr_t evt, u8 operation, u64 val)
-{
-	struct kbase_device *kbdev = queue->kctx->kbdev;
-
-	switch (operation) {
-	case BASEP_CQS_SET_OPERATION_ADD:
-		*(u32 *)evt += (u32)val;
-		break;
-	case BASEP_CQS_SET_OPERATION_SET:
-		*(u32 *)evt = val;
-		break;
-	default:
-		dev_dbg(kbdev->dev, "Unsupported CQS set operation %d", operation);
-		queue->has_error = true;
-		break;
-	}
-}
-
-static void kbasep_kcpu_cqs_do_set_operation_64(struct kbase_kcpu_command_queue *queue,
-						uintptr_t evt, u8 operation, u64 val)
-{
-	struct kbase_device *kbdev = queue->kctx->kbdev;
-
-	switch (operation) {
-	case BASEP_CQS_SET_OPERATION_ADD:
-		*(u64 *)evt += val;
-		break;
-	case BASEP_CQS_SET_OPERATION_SET:
-		*(u64 *)evt = val;
-		break;
-	default:
-		dev_dbg(kbdev->dev, "Unsupported CQS set operation %d", operation);
-		queue->has_error = true;
-		break;
-	}
-}
-
 static void kbase_kcpu_cqs_set_operation_process(
 		struct kbase_device *kbdev,
 		struct kbase_kcpu_command_queue *queue,
@@ -1194,9 +1094,9 @@ static void kbase_kcpu_cqs_set_operation_process(
 
 	for (i = 0; i < cqs_set_operation->nr_objs; i++) {
 		struct kbase_vmap_struct *mapping;
-		uintptr_t evt;
+		u64 *evt;
 
-		evt = (uintptr_t)kbase_phy_alloc_mapping_get(
+		evt = (u64 *)kbase_phy_alloc_mapping_get(
 			queue->kctx, cqs_set_operation->objs[i].addr, &mapping);
 
 		/* GPUCORE-28172 RDT to review */
@@ -1206,31 +1106,38 @@ static void kbase_kcpu_cqs_set_operation_process(
 				"Sync memory %llx already freed", cqs_set_operation->objs[i].addr);
 			queue->has_error = true;
 		} else {
-			struct base_cqs_set_operation_info *obj = &cqs_set_operation->objs[i];
-
-			switch (obj->data_type) {
+			switch (cqs_set_operation->objs[i].operation) {
+			case BASEP_CQS_SET_OPERATION_ADD:
+				*evt += cqs_set_operation->objs[i].val;
+				break;
+			case BASEP_CQS_SET_OPERATION_SET:
+				*evt = cqs_set_operation->objs[i].val;
+				break;
 			default:
-				WARN_ON(!kbase_kcpu_cqs_is_data_type_valid(obj->data_type));
+				dev_warn(kbdev->dev,
+					"Unsupported CQS set operation %d", cqs_set_operation->objs[i].operation);
 				queue->has_error = true;
-				goto skip_err_propagation;
+				break;
+			}
+
+			/* Increment evt up to the error_state value depending on the CQS data type */
+			switch (cqs_set_operation->objs[i].data_type) {
+			default:
+				dev_warn(kbdev->dev, "Unreachable data_type=%d", cqs_set_operation->objs[i].data_type);
+			/* Fallthrough - hint to compiler that there's really only 2 options at present */
 			case BASEP_CQS_DATA_TYPE_U32:
-				kbasep_kcpu_cqs_do_set_operation_32(queue, evt, obj->operation,
-								    obj->val);
-				evt += BASEP_EVENT32_ERR_OFFSET - BASEP_EVENT32_VAL_OFFSET;
+				evt = (u64 *)((u8 *)evt + sizeof(u32));
 				break;
 			case BASEP_CQS_DATA_TYPE_U64:
-				kbasep_kcpu_cqs_do_set_operation_64(queue, evt, obj->operation,
-								    obj->val);
-				evt += BASEP_EVENT64_ERR_OFFSET - BASEP_EVENT64_VAL_OFFSET;
+				evt = (u64 *)((u8 *)evt + sizeof(u64));
 				break;
 			}
 
 			/* GPUCORE-28172 RDT to review */
 
 			/* Always propagate errors */
-			*(u32 *)evt = queue->has_error;
+			*evt = queue->has_error;
 
-skip_err_propagation:
 			kbase_phy_alloc_mapping_put(queue->kctx, mapping);
 		}
 	}
@@ -1249,7 +1156,6 @@ static int kbase_kcpu_cqs_set_operation_prepare(
 	struct kbase_context *const kctx = kcpu_queue->kctx;
 	struct base_cqs_set_operation_info *objs;
 	unsigned int nr_objs = cqs_set_operation_info->nr_objs;
-	unsigned int i;
 
 	lockdep_assert_held(&kctx->csf.kcpu_queues.lock);
 
@@ -1267,18 +1173,6 @@ static int kbase_kcpu_cqs_set_operation_prepare(
 			nr_objs * sizeof(*objs))) {
 		kfree(objs);
 		return -ENOMEM;
-	}
-
-	/* Check the CQS objects as early as possible. By checking their alignment
-	 * (required alignment equals to size for Sync32 and Sync64 objects), we can
-	 * prevent overrunning the supplied event page.
-	 */
-	for (i = 0; i < nr_objs; i++) {
-		if (!kbase_kcpu_cqs_is_data_type_valid(objs[i].data_type) ||
-		    !kbase_kcpu_cqs_is_aligned(objs[i].addr, objs[i].data_type)) {
-			kfree(objs);
-			return -EINVAL;
-		}
 	}
 
 	current_command->type = BASE_KCPU_COMMAND_TYPE_CQS_SET_OPERATION;
